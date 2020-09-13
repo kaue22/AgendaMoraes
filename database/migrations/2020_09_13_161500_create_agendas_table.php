a862a4aaecedc8615b3aa16e17af301d4743c26a
Kaue <kauegouveia@gmail.com>
2020-09-13 13:29:29 -0300

    Criação de rota,controller e view pós autenticação do usuário

---
+++ database/migrations/2020_09_13_161500_create_agendas_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAgendasTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('agendas', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('name')->unique();
+            $table->string('phone');
+            $table->string('state');
+            $table->string('city');
+            $table->string('info');
+            $table->string('categoria');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('agendas');
+    }
+}
